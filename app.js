e19f0352c7d233bf191f0273906e0e9dab529cde
navingp <navingp@gmail.com>
2021-10-30 17:38:34 +0530

    Commit Again

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
 app.get('/S1', (req, res) => {
     res.send('Hello World! S1')
